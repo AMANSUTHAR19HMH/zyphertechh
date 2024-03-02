e2fda086ebcff7feefd91b9107ca439d0372433b
janhviwadhwa <janhviwadhwa972@gmail.com>
2024-03-02 16:05:26 +0530

    home page 2 and patient page2

---
+++ app/src/main/java/com/example/page1/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.page1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+      /*  Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r0sy4rvqx6pqa))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.ri0kvxssr6ih))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rqn2ynhduhw))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rabbhzfwe7y))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rny14unslgvc))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rrvbi7clkc9g))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rwg0yenx26f))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r466ikkvu6bx))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r49mnqegleo3))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rlg3vyqpqlj))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r2don99fk0tz))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rh1gcgmjrun))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rbyvmgtro8r))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rdrfyw6m2vuw))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r7ecr9g2ez69))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.ravqoz8i8tb5))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rlkxf2r6gcbl))
+        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.r6tw1m4jyr2o))*/
+    }
+}
---
+++ app/src/main/java/com/example/page1/page2.kt
@@ -0,0 +1,45 @@
+package com.example.page1
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.page1.databinding.ActivityPage2Binding
+
+class page2 : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityPage2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_page2)
+
+           /* Glide.with(this).load("https://i.imgur.com/1tMFzp8.png")
+                .into(findViewById(R.id.rlhxd3u3x4qn))
+            Glide.with(this).load("https://i.imgur.com/1tMFzp8.png")
+                .into(findViewById(R.id.rn9e1zvr4ri))
+            Glide.with(this).load("https://i.imgur.com/1tMFzp8.png")
+                .into(findViewById(R.id.ru66yl9zb56))
+            Glide.with(this).load("https://i.imgur.com/1tMFzp8.png")
+                .into(findViewById(R.id.rpw3wpeymz6))
+            Glide.with(this).load("https://i.imgur.com/1tMFzp8.png")
+                .into(findViewById(R.id.r1mv346scb6f))
+            Glide.with(this).load("https://i.imgur.com/1tMFzp8.png")
+                .into(findViewById(R.id.rc0ybfhkscjf))
+            Glide.with(this).load("https://i.imgur.com/1tMFzp8.png")
+                .into(findViewById(R.id.r0ah8v8cl6ulo))
+            Glide.with(this).load("https://i.imgur.com/1tMFzp8.png")
+                .into(findViewById(R.id.rpqvls7427x))
+            Glide.with(this).load("https://i.imgur.com/1tMFzp8.png")
+                .into(findViewById(R.id.rfu4hohmmewr))
+            Glide.with(this).load("https://i.imgur.com/1tMFzp8.png")
+                .into(findViewById(R.id.rggf0c7r09v9))*/
+        }
+    }
+
+
