83d773bdd77ad570247a037ce012ff0ccf5dbd35
janhviwadhwa <janhviwadhwa972@gmail.com>
2024-03-02 17:58:09 +0530

    home page 2 and patient page2 patient and odc login

---
+++ app/src/main/java/com/example/page1/devicelogin.kt
@@ -0,0 +1,17 @@
+package com.example.page1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class devicelogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_devicelogin)
+
+//        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rslwhd0sw2gj))
+//        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rulftfkfyuu))
+//        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rjy9pck9tfur))
+//
+//
+    }
+}
---
+++ app/src/main/java/com/example/page1/doctorlogin.kt
@@ -0,0 +1,18 @@
+package com.example.page1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class doctorlogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_doctorlogin)
+
+//        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rjkk8gg13yj))
+//        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rryosqcvrcd))
+//        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rnuwilmc9lz))
+//
+//
+
+    }
+}
